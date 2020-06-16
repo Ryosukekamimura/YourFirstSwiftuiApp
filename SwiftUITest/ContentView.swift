b9264126a8a909a79af8090704857a66ab430c5d
RyosukeKamimura <come1234560@gmail.com>
2020-06-17 08:23:32 +0900

    learn HStack and VStack

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -10,9 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            HStack(alignment: .top, spacing: 20, content: {
+                Text("apple.com")
+                
+                Image("lemon")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                Button(action: {
+                    //TODO:
+                }, label: {
+                    Image("apple")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                })
+                Spacer()
+            })
+            Spacer()
+            Text("by kamimura ryousuke")
+        }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
