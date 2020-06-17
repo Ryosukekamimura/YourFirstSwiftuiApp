f16fd77603a5df0952be85c51f92f5d104a8801a
RyosukeKamimura <come1234560@gmail.com>
2020-06-17 09:02:20 +0900

    modify me!

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -10,58 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
-        VStack{
-            Spacer()
-            HStack{
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-            }
-            Spacer()
-            HStack{
-                   Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-            }
-            Spacer()
-            HStack{
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-                Image("apple")
-                    .resizable()
-                    .frame(width:100 , height: 100)
-                Spacer()
-            }
-            Spacer()
-        }
+        
+        Text("Modify ME!")
+            .font(.headline)
+            .fontWeight(.heavy)
+            .foregroundColor(Color.pink)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 23.0)
 
     }
 }
