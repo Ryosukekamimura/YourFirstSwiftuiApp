43e1a9a0369546dac769f8ce4de517d969bdee6b
RyosukeKamimura <come1234560@gmail.com>
2020-06-18 15:35:50 +0900

    SF symbols

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -9,52 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isHearted = false
-    @State private var numHearted = 999
+
     var body: some View {
         
-        VStack{
-            Image("pinkbuilding")
-                .resizable()
-                .cornerRadius(30)
-                .padding()
-                .shadow(radius: 10)
-            
-            HStack{
-                Button(action: {
-                    self.isHearted.toggle()
-                    if self.isHearted{
-                        self.numHearted += 1
-                    }else{
-                        self.numHearted -= 1
-                    }
-                }, label: {
-                    if isHearted{
-                        Image(systemName: "heart.fill")
-                    }else{
-                        Image(systemName: "heart")
-                    }
-                        
-                })
-                
-                Text(String(numHearted))
-                
-            }
-            
-
-            
-            
-            Text("pink building is totally amazing.")
-                .fontWeight(.heavy)
-                .foregroundColor(Color.pink)
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .shadow(color: .black, radius: 0.1, x: 2, y: 1)
-            
-        }
-        
-        
-        
+        HStack{
+            Image(systemName: "mic.circle.fill")
+            Text("Likes")
+        }.font(.largeTitle)
         
         
         
